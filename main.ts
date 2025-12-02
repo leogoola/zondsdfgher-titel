e68f9a377cb74ad6850b348e23b550fdbc2c4175
leogoola <leonmartens2012@outlook.com>
2025-12-02 15:36:59 +0100

    Initiële bestanden voor MakeCode-project

---
+++ main.ts
@@ -0,0 +1,38 @@
+basic.forever(function () {
+    if (input.compassHeading() > 315 || input.compassHeading() < 45) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+    if (input.compassHeading() > 135 && input.compassHeading() < 225) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    }
+    if (input.compassHeading() < 315 && input.compassHeading() > 225) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    }
+    if (input.compassHeading() > 45 && input.compassHeading() < 135) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    }
+})
